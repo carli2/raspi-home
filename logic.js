11233986dae7701f9d4915c20eeba79619d8a101
Philipp Lätsch <philippl@launix.de>
2017-01-06 14:46:29 +0100

    Layout-Anpassung

--- logic.js
+++ logic.js
@@ -3,10 +3,10 @@ var lights = require('./lights.js');
 module.exports.page = function () {
 	var html = '<div class=\"container-fluid\">';
 	html += '<div class=\"buttons btn-group-lg\">';
-	html += '<h3>Flur Licht:</h3>  <a class=\"btn-primary btn-large btn\" href=\"/action/lighton/0\">an</a> <a class=\"btn-primary btn btn-large\" href=\"/action/lightoff/0\">aus</a> <br>';
-	html += '<h3>Flur Hinten:</h3>  <a class=\"btn-primary btn-large btn\" href=\"/action/lighton/1\">an</a> <a class=\"btn-primary btn btn-large\" href=\"/action/lightoff/1\">aus</a> <br>';
-	html += '<h3>Flur Draussen:</h3>  <a class=\"btn-primary btn-large btn\" href=\"/action/lighton/2\">an</a> <a class=\"btn-primary btn btn-large\" href=\"/action/lightoff/2\">aus</a> <br>';
-	html += '<h3>Flur Stube:</h3>  <a class=\"btn-primary btn-large btn\" href=\"/action/lighton/3\">an</a> <a class=\"btn-primary btn btn-large\" href=\"/action/lightoff/3\">aus</a> <br>';
+	html += '<h3 class="links">Flur Licht:</h3>  <a class=\"btn-primary btn-large btn rechts\" href=\"/action/lighton/0\">an</a> <a class=\"btn-primary btn btn-large rechts\" href=\"/action/lightoff/0\">aus</a> <br class="clear">';
+	html += '<h3 class="links">Flur Hinten:</h3>  <a class=\"btn-primary btn-large btn rechts\" href=\"/action/lighton/1\">an</a> <a class=\"btn-primary btn btn-large rechts\" href=\"/action/lightoff/1\">aus</a> <br class="clear">';
+	html += '<h3 class="links">Flur Draussen:</h3>  <a class=\"btn-primary btn-large btn rechts\" href=\"/action/lighton/2\">an</a> <a class=\"btn-primary btn btn-large rechts\" href=\"/action/lightoff/2\">aus</a> <br class="clear">';
+	html += '<h3 class="links">Flur Stube:</h3>  <a class=\"btn-primary btn-large btn rechts\" href=\"/action/lighton/3\">an</a> <a class=\"btn-primary btn btn-large rechts\" href=\"/action/lightoff/3\">aus</a> <br class="clear">';
 	html += '<h3>Blinken:</h3> <a class=\"btn-primary btn-large btn-block btn\" href=\"/action/blink\">Click!</a> <br>';
 	return html;
 }
